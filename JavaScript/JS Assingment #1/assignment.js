49b99fe1bc37c1630d4bcf5b29c747875ea872a0
Kurbyy <edgarrogel98@yahoo.com>
2021-08-23 17:58:42 -0500

    JS assignment

---
+++ JavaScript/JS Assingment #1/assignment.js
@@ -0,0 +1,29 @@
+//#1
+var num1 = 0;
+var num2 = 2;
+var num3 = 0;
+var num4 = 2;
+
+console.log(num1 / num2)
+
+var str1 = "This is my first \"ever\" string";
+
+console.log(str1);
+
+var myVar = 0;
+var myVar = true;
+var myVar = null;
+var myVar = undefined;
+var myVar = "Edgar";
+
+var animal = {
+    type: "tiger",
+    name: "tony",
+    color: "orange",
+    weight: 300,
+    diet: "carne",
+    var activities = new Array("running", "hunting", "playing");
+  
+};
+
+console.log(animal);
